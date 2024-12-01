437a3726293f06157da369f9353d1a536cc6c80a
Bryan Eddy <b.s.eddy@gmail.com>
2024-11-30 19:26:20 -0800

    Added input validation and error handling. Ensured division function cannot divide by zero. Modified structure to allow for multiple calculations.

--- main.py
+++ main.py
@@ -17,43 +17,72 @@ def mul(a,b):
 
 #This function divides both numbers
 def div(a,b):
+    
+    # validation for dividing by zero
+    if b == 0:
+        print("Can't divide by 0, try another number")
+        return
+    
     answer = a / b
     print(str(a) + "/" + str(b) + "=" + str(answer))
+    
+# added while loop to allow for multiple calculator operations
+
+while True:
+    print("A. Addition")
+    print("B. Subtraction")
+    print("C. Multiplication")
+    print("D. Division")
+    print("E. Exit ")
+
+    #Choice aids users to define function per functions listed prior
+    choice = input("Input your choice:")
+    
+    # restructured choices with exit condition choice first
+    if choice == "e" or choice == "E":
+        print("Program has ended")
+        break
+    
+    # input validation for any input not defined
+    if choice not in ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D']:
+        print("Invalid choice, please select again.")
+        continue
+    
+    # float conversion with input validation on user number inputs
+    try:
+        a = float(input("Input the first number:"))
+        b = float(input("Input the second number:"))
+    except ValueError:
+        print("Invalid input, please use a valid number.")
+        continue
+
+    if choice == "a" or choice == "A":
+        print("Addition")
+        add(a,b)
+        
+    elif choice == "b" or choice == "B":
+        print("Subtraction")
+        sub(a,b)
+        
+    elif choice == "c" or choice == "C":
+        print("Multiplication")
+        mul(a,b)
+        
+    elif choice == "d" or choice == "D":
+        print("Division")
+        div(a,b)
+        
+    # see if user wants to perform another operation or quit
+    
+    calc_continue = input("Do you want to perform another calculation? (y/n): ")
+    if calc_continue == "y" or calc_continue == "Y":
+    # loop will start again
+        pass
+    # any other input other than y or Y will result in program termination
+    else:
+        print("Program has ended")
+        break
+    
 
 
-print("A. Addition")
-print("B. Subtraction")
-print("C. Multiplication")
-print("D. Division")
-print("E. Exit ")
 
-#Choice aids users to define function per functions listed prior
-choice = input("Input your choice:")
-
-if choice == "a" or choice == "A":
-    print("Addition")
-    a = int(input("Input the first number:"))
-    b = int(input("Input the second number:"))
-    add(a,b)
-
-elif choice == "b" or choice == "B":
-    print("Subtraction")
-    a = int(input("Input the first number:"))
-    b = int(input("Input the second number:"))
-    sub(a,b)
-
-elif choice == "c" or choice == "C":
-    print("Multiplication")
-    a = int(input("Input the first number:"))
-    b = int(input("Input the second number:"))
-    mul(a,b)
-
-elif choice == "d" or choice == "D":
-    print("Division")
-    a = int(input("Input the first number:"))
-    b = int(input("Input the second number:"))
-    div(a,b)
-
-elif choice == "e" or choice == "E":
-    print("Program has ended")
-    quit()
